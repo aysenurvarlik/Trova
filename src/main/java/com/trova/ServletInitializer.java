0b74a0366db7766a4e959b3d17f3640591d1ad8d
AysenurVarlik <aysenurvarlik96@gmail.com>
2020-05-26 02:46:55 +0300

    Initial Commit

---
+++ src/main/java/com/trova/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.trova;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TrovaApplication.class);
+	}
+
+}
